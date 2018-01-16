025f864c6693cba416eb182d404f65c81ffda48c
Lucianoedgard <lucianoedgard@hotmail.com>
2018-01-16 19:32:35 -0200

    Initial commit

--- CadCli.Domain/ClientType.cs
+++ CadCli.Domain/ClientType.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace CadCli.Domain
 {
@@ -8,11 +9,15 @@ namespace CadCli.Domain
         {
             CreationDate = DateTime.Now;
         }
-
+        [Key]
         public int Id { get; set; }
+        [Display(Name = "Código")]
         public string Code { get; set; }
+        [Display(Name ="Descrição")]
         public string Description { get; set; }
+        [Display(Name ="Ativo")]
         public bool IsActive { get; set; }
+        [Display(Name ="Data de criação")]
         public DateTime CreationDate { get; set; }
     }
 }
---
+++ CadCli.WebApp/WebApp/Controllers/ClientTypesController.cs
@@ -0,0 +1,124 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using CadCli.Domain;
+using CadCli.Infra.DataContext;
+
+namespace WebApp.Controllers
+{
+    public class ClientTypesController : Controller
+    {
+        private CadCliDataContext db = new CadCliDataContext();
+
+        // GET: ClientTypes
+        public ActionResult Index()
+        {
+            return View(db.ClientType.ToList());
+        }
+
+        // GET: ClientTypes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ClientType clientType = db.ClientType.Find(id);
+            if (clientType == null)
+            {
+                return HttpNotFound();
+            }
+            return View(clientType);
+        }
+
+        // GET: ClientTypes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ClientTypes/Create
+        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
+        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Code,Description,IsActive,CreationDate")] ClientType clientType)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ClientType.Add(clientType);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(clientType);
+        }
+
+        // GET: ClientTypes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ClientType clientType = db.ClientType.Find(id);
+            if (clientType == null)
+            {
+                return HttpNotFound();
+            }
+            return View(clientType);
+        }
+
+        // POST: ClientTypes/Edit/5
+        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
+        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Code,Description,IsActive,CreationDate")] ClientType clientType)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(clientType).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(clientType);
+        }
+
+        // GET: ClientTypes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ClientType clientType = db.ClientType.Find(id);
+            if (clientType == null)
+            {
+                return HttpNotFound();
+            }
+            return View(clientType);
+        }
+
+        // POST: ClientTypes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ClientType clientType = db.ClientType.Find(id);
+            db.ClientType.Remove(clientType);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
